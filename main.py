376eb92f7e398e35e4e99d595c5df7e33a48568c
Ravonne Byrd <rnevels10@gmail.com>
2021-06-21 21:27:12 -0400

    finished game, needs README and styling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from KubaGame import KubaGame, Player, Board
-
+pygame.font.init()
 
 # setting the window
 WIDTH, HEIGHT = 1200, 800
@@ -24,6 +24,11 @@ MARGIN_Y = 113
 SQUARE_WIDTH = SQUARE_HEIGHT = 80
 SQUARE_MARGIN = 2
 COLUMNS = ROWS = 7
+
+# font constants
+CAPTURES_FONT = MARBLES_FONT = pygame.font.SysFont('helvetica', 20)
+WINNER_FONT = pygame.font.SysFont('lucidagrande', 80)
+
 
 FPS = 60
 
@@ -57,16 +62,47 @@ def draw_grid(grid):
         top = top + SQUARE_HEIGHT + SQUARE_MARGIN
 
 
-def setup_window(grid):
+def draw_indicators(game, winner):
+    turn = None
+    won = None
+    player_a = CAPTURES_FONT.render("Player A - White", True, WHITE)
+    player_b = CAPTURES_FONT.render("Player B - Black", True, WHITE)
+    player_a_captures = CAPTURES_FONT.render("Red Captures: " + str(game.get_player_a().get_captured()), True, RED)
+    player_b_captures = CAPTURES_FONT.render("Red Captures: " + str(game.get_player_b().get_captured()), True, RED)
+    player_a_o_cap = CAPTURES_FONT.render("Black Captures: " + str(8 - game.get_board().get_count_black()), True, RED)
+    player_b_o_cap = CAPTURES_FONT.render("White Captures: " + str(8 - game.get_board().get_count_white()), True, RED)
+
+    if game.get_current_turn() == game.get_player_a().get_name():
+        turn = CAPTURES_FONT.render("Turn: White", True, WHITE)
+    elif game.get_current_turn() == game.get_player_b().get_name():
+        turn = CAPTURES_FONT.render("Turn: Black", True, WHITE)
+
+    if winner == game.get_player_a().get_name():
+        won = WINNER_FONT.render("WINNER: White", True, RED)
+    elif winner == game.get_player_b().get_name():
+        won = WINNER_FONT.render("WINNER: Black", True, RED)
+
+    WINDOW.blit(player_a, (70, 300))
+    WINDOW.blit(player_b, (950, 300))
+    WINDOW.blit(player_a_captures, (70, 340))
+    WINDOW.blit(player_b_captures, (950, 340))
+    WINDOW.blit(player_a_o_cap, (70, 370))
+    WINDOW.blit(player_b_o_cap, (950, 370))
+    if turn is not None:
+        WINDOW.blit(turn, (545, 700))
+    if won is not None:
+        WINDOW.blit(won, (350, 350))
+
+
+def setup_window(grid, game, winner):
     WINDOW.fill(BACKGROUND_COLOR)
     draw_grid(grid)
+    draw_indicators(game, winner)
     pygame.display.update()
 
 
-def make_move(game, player, coordinates, direction_coordinates):
+def make_move(game, player, row_md, column_md, row_mu, column_mu):
     direction = None
-    row_md, column_md = coordinates
-    row_mu, column_mu = direction_coordinates
 
     if row_mu > row_md:
         direction = 'B'
@@ -77,7 +113,7 @@ def make_move(game, player, coordinates, direction_coordinates):
     if column_mu < column_md:
         direction = 'L'
 
-    return game.make_move(player, coordinates, direction)
+    return game.make_move(player, (row_md, column_md), direction)
 
 
 def main():
@@ -85,16 +121,10 @@ def main():
     clock = pygame.time.Clock()
 
     game = KubaGame(('player_a', 'W'), ('player_b', 'B'))
-    setup_window(game.get_board().get_board())
-    turn = 0
+    setup_window(game.get_board().get_board(), game, game.get_winner())
 
-    position_md = None
     column_new_md = None
     row_new_md = None
-
-    position_mu = None
-    column_new_mu = None
-    row_new_mu = None
 
     while run:
         clock.tick(FPS)
@@ -117,17 +147,17 @@ def main():
 
                 if row_new_md != row_new_mu or column_new_md != column_new_mu:
                     if game.get_marble((row_new_md, column_new_md)) == 'W':
-                        if make_move(game, 'player_a', (row_new_md, column_new_md), (row_new_mu, column_new_mu)):
-                            setup_window(game.get_board().get_board())
+                        if make_move(game, 'player_a', row_new_md, column_new_md, row_new_mu, column_new_mu):
+                            setup_window(game.get_board().get_board(), game, game.get_winner())
 
                     elif game.get_marble((row_new_md, column_new_md)) == 'B':
-                        if make_move(game, 'player_b', (row_new_md, column_new_md), (row_new_mu, column_new_mu)):
-                            setup_window(game.get_board().get_board())
+                        if make_move(game, 'player_b', row_new_md, column_new_md, row_new_mu, column_new_mu):
+                            setup_window(game.get_board().get_board(), game, game.get_winner())
 
         if game.get_winner() is not None:
-            run = False
+            setup_window(game.get_board().get_board(), game, game.get_winner())
 
-    pygame.quit()
+    main()
 
 
 if __name__ == "__main__":
